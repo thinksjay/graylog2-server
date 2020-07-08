700f6e6ff72ed8ca14da4f44cd86cc618fd6e4e6
Kyle Knight <kyle@graylog.com>
2020-07-08 13:41:46 -0500

    implement new colors

--- graylog2-web-interface/src/components/graylog/FormControl.jsx
+++ graylog2-web-interface/src/components/graylog/FormControl.jsx
@@ -1,35 +1,34 @@
 // eslint-disable-next-line no-restricted-imports
 import { FormControl as BootstrapFormControl } from 'react-bootstrap';
 import styled, { css } from 'styled-components';
-import chroma from 'chroma-js';
 
-const FormControl = styled(BootstrapFormControl)(({ theme }) => {
-  return css`
-    color: ${theme.colors.global.textDefault};
-    background-color: ${theme.colors.global.contentBackground};
-    border-color: ${theme.colors.gray[80]};
+const FormControl = styled(BootstrapFormControl)(({ theme }) => css`
+  &.form-control {
+    color: ${theme.colors.input.color};
+    background-color: ${theme.colors.input.background};
+    border-color: ${theme.colors.input.border};
 
     &::placeholder {
-      color: ${theme.colors.gray[60]};
+      color: ${theme.colors.input.placeholder};
     }
 
     &:focus {
-      border-color: ${theme.colors.variant.light.info};
-      box-shadow: inset 0 1px 1px rgba(0, 0, 0, 0.075),
-        0 0 8px ${chroma(theme.colors.variant.light.info).alpha(0.4).css()};
+      border-color: ${theme.colors.input.borderFocus};
+      box-shadow: ${theme.colors.input.boxShadow};
     }
 
     &[disabled],
     &[readonly],
     fieldset[disabled] & {
-      background-color: ${theme.colors.gray[80]};
+      background-color: ${theme.colors.input.backgroundDisabled};
+      color: ${theme.colors.input.colorDisabled};
     }
 
     ~ .form-control-feedback.glyphicon {
       display: none;
     }
-  `;
-});
+  }
+`);
 
 FormControl.Static = BootstrapFormControl.Static;
 FormControl.Feedback = BootstrapFormControl.Feedback;
